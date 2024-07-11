36d9fb83725dfaab7b0ade35919edd72d214f76e
Anna Kirillova <a.kirillova1812@gmail.com>
2024-07-11 14:12:50 +0400

    Refactoring

--- src/main/java/com/kirillova/gymcrmsystem/util/DataLoaderUtil.java
+++ src/main/java/com/kirillova/gymcrmsystem/util/DataLoaderUtil.java
@@ -4,15 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.function.Consumer;
 
 public class DataLoaderUtil {
     private static final Logger log = LoggerFactory.getLogger(DataLoaderUtil.class);
 
     public static void loadData(String filePath, Consumer<String[]> lineProcessor) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        ClassLoader classLoader = DataLoaderUtil.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(filePath);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
