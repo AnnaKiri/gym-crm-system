e487390e3b0aa8fcf50793a67cbabb3cb7e158e0
Anna Kirillova <a.kirillova1812@gmail.com>
2024-07-16 17:56:38 +0400

    Fix TestData

--- src/test/java/com/kirillova/gymcrmsystem/TestData.java
+++ src/test/java/com/kirillova/gymcrmsystem/TestData.java
@@ -6,7 +6,7 @@ import com.kirillova.gymcrmsystem.models.Training;
 import com.kirillova.gymcrmsystem.models.TrainingType;
 import com.kirillova.gymcrmsystem.models.User;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 public class TestData {
     public static final User user1 = new User(1, "Angelina", "Jolie", "Angelina.Jolie", "password1", true);
@@ -27,27 +27,27 @@ public class TestData {
     public static final TrainingType trainingType4 = new TrainingType(4, "Stretching");
     public static final TrainingType newTrainingType = new TrainingType(5, "New Training Type");
 
-    public static final Trainee trainee1 = new Trainee(1, LocalDate.of(1975, 6, 4), "some address", user1.getId());
-    public static final Trainee trainee2 = new Trainee(2, LocalDate.of(1976, 10, 23), "some address", user2.getId());
-    public static final Trainee trainee3 = new Trainee(3, LocalDate.of(1976, 9, 15), "some address", user3.getId());
-    public static final Trainee trainee4 = new Trainee(4, LocalDate.of(1964, 9, 2), "some address", user4.getId());
-    public static final Trainee newTrainee = new Trainee(5, LocalDate.of(1969, 11, 4), "some new address", newUser.getId());
-    public static final Trainee updatedTrainee = new Trainee(1, LocalDate.of(1976, 4, 10), "updated address", updatedUserForTrainee.getId());
+    public static final Trainee trainee1 = new Trainee(1, new Date(1975, 6, 4), "some address", user1);
+    public static final Trainee trainee2 = new Trainee(2, new Date(1976, 10, 23), "some address", user2);
+    public static final Trainee trainee3 = new Trainee(3, new Date(1976, 9, 15), "some address", user3);
+    public static final Trainee trainee4 = new Trainee(4, new Date(1964, 9, 2), "some address", user4);
+    public static final Trainee newTrainee = new Trainee(5, new Date(1969, 11, 4), "some new address", newUser);
+    public static final Trainee updatedTrainee = new Trainee(1, new Date(1976, 4, 10), "updated address", updatedUserForTrainee);
 
-    public static final Trainer trainer1 = new Trainer(1, trainingType1.getId(), user5.getId());
-    public static final Trainer trainer2 = new Trainer(2, trainingType2.getId(), user6.getId());
-    public static final Trainer trainer3 = new Trainer(3, trainingType3.getId(), user7.getId());
-    public static final Trainer trainer4 = new Trainer(4, trainingType4.getId(), user8.getId());
-    public static final Trainer newTrainer = new Trainer(5, trainingType4.getId(), newUser.getId());
-    public static final Trainer updatedTrainer = new Trainer(1, trainingType2.getId(), updatedUserForTrainer.getId());
+    public static final Trainer trainer1 = new Trainer(1, trainingType1, user5);
+    public static final Trainer trainer2 = new Trainer(2, trainingType2, user6);
+    public static final Trainer trainer3 = new Trainer(3, trainingType3, user7);
+    public static final Trainer trainer4 = new Trainer(4, trainingType4, user8);
+    public static final Trainer newTrainer = new Trainer(5, trainingType4, newUser);
+    public static final Trainer updatedTrainer = new Trainer(1, trainingType2, updatedUserForTrainer);
 
-    public static final Training training1 = new Training(1, trainee1.getId(), trainer4.getId(), "Stretching", trainingType4.getId(), LocalDate.of(2024, 1, 1), 60);
-    public static final Training training2 = new Training(2, trainee1.getId(), trainer2.getId(), "Aerobic", trainingType2.getId(), LocalDate.of(2024, 1, 2), 60);
-    public static final Training training3 = new Training(3, trainee2.getId(), trainer2.getId(), "Aerobic", trainingType2.getId(), LocalDate.of(2024, 1, 2), 60);
-    public static final Training training4 = new Training(4, trainee2.getId(), trainer3.getId(), "Yoga", trainingType3.getId(), LocalDate.of(2024, 1, 5), 60);
-    public static final Training training5 = new Training(5, trainee3.getId(), trainer1.getId(), "Strength", trainingType1.getId(), LocalDate.of(2024, 1, 5), 60);
-    public static final Training training6 = new Training(6, trainee4.getId(), trainer4.getId(), "Stretching", trainingType4.getId(), LocalDate.of(2024, 1, 1), 60);
-    public static final Training training7 = new Training(7, trainee4.getId(), trainer1.getId(), "Strength", trainingType1.getId(), LocalDate.of(2024, 1, 6), 60);
-    public static final Training training8 = new Training(8, trainee2.getId(), trainer3.getId(), "Yoga", trainingType3.getId(), LocalDate.of(2024, 1, 5), 60);
-    public static final Training newTraining = new Training(9, trainee3.getId(), trainer3.getId(), "Yoga", trainingType3.getId(), LocalDate.of(2024, 1, 5), 60);
+    public static final Training training1 = new Training(1, trainee1, trainer4, "Stretching", trainingType4, new Date(2024, 1, 1), 60);
+    public static final Training training2 = new Training(2, trainee1, trainer2, "Aerobic", trainingType2, new Date(2024, 1, 2), 60);
+    public static final Training training3 = new Training(3, trainee2, trainer2, "Aerobic", trainingType2, new Date(2024, 1, 2), 60);
+    public static final Training training4 = new Training(4, trainee2, trainer3, "Yoga", trainingType3, new Date(2024, 1, 5), 60);
+    public static final Training training5 = new Training(5, trainee3, trainer1, "Strength", trainingType1, new Date(2024, 1, 5), 60);
+    public static final Training training6 = new Training(6, trainee4, trainer4, "Stretching", trainingType4, new Date(2024, 1, 1), 60);
+    public static final Training training7 = new Training(7, trainee4, trainer1, "Strength", trainingType1, new Date(2024, 1, 6), 60);
+    public static final Training training8 = new Training(8, trainee2, trainer3, "Yoga", trainingType3, new Date(2024, 1, 5), 60);
+    public static final Training newTraining = new Training(9, trainee3, trainer3, "Yoga", trainingType3, new Date(2024, 1, 5), 60);
 }
