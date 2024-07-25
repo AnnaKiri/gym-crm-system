f179938797764bfe71493b2cc503fbdd772690a1
Anna Kirillova <a.kirillova1812@gmail.com>
2024-07-25 23:03:30 +0400

    Tests for TrainingTypeController were added

---
+++ src/test/java/com/kirillova/gymcrmsystem/web/trainingtype/TrainingTypeControllerTest.java
@@ -0,0 +1,24 @@
+package com.kirillova.gymcrmsystem.web.trainingtype;
+
+import com.kirillova.gymcrmsystem.AbstractSpringTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.kirillova.gymcrmsystem.TrainingTypeTestData.TRAINING_TYPE_LIST;
+import static com.kirillova.gymcrmsystem.TrainingTypeTestData.TRAINING_TYPE_MATCHER;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TrainingTypeControllerTest extends AbstractSpringTest {
+    private static final String REST_URL = TrainingTypeController.REST_URL + '/';
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(TRAINING_TYPE_MATCHER.contentJson(TRAINING_TYPE_LIST));
+    }
+
+}
