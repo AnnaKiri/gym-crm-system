0dd6096cb88703d24bc1a9b31c5363a4072ae29b
Anna Kirillova <a.kirillova1812@gmail.com>
2024-09-25 17:25:51 +0400

    Resilience4j circuitbreaker was added

--- src/main/java/com/kirillova/gymcrmsystem/feign/TrainerWorkloadServiceFeignClient.java
+++ src/main/java/com/kirillova/gymcrmsystem/feign/TrainerWorkloadServiceFeignClient.java
@@ -1,6 +1,9 @@
 package com.kirillova.gymcrmsystem.feign;
 
 import com.kirillova.gymcrmsystem.dto.TrainingInfoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient("trainer-workload-service")
 public interface TrainerWorkloadServiceFeignClient {
 
+    Logger log = LoggerFactory.getLogger(TrainerWorkloadServiceFeignClient.class);
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/trainings")
+    @CircuitBreaker(name = "trainerWorkloadService", fallbackMethod = "updateTrainingInfoFallbackMethod")
     void updateTrainingInfo(@RequestBody TrainingInfoDto trainingInfoDto);
+
+    default void updateTrainingInfoFallbackMethod(Throwable throwable) {
+        log.error("TrainerWorkloadService sent an error response: {}", throwable.getMessage(), throwable);
+    }
 }
