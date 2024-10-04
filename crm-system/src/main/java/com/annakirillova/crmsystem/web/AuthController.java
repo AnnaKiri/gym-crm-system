a79a8fc7efca7fc99459258b20014f593dd2e364
Anna Kirillova <a.kirillova1812@gmail.com>
2024-10-04 18:16:20 +0400

    Refactoring Auth Controller

--- crm-system/src/main/java/com/annakirillova/crmsystem/web/AuthController.java
+++ crm-system/src/main/java/com/annakirillova/crmsystem/web/AuthController.java
@@ -5,6 +5,11 @@ import com.annakirillova.crmsystem.dto.TokenResponseDto;
 import com.annakirillova.crmsystem.service.AuthService;
 import com.annakirillova.crmsystem.service.BruteForceProtectionService;
 import com.annakirillova.crmsystem.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -19,7 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping(value = AuthController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(AuthController.REST_URL)
+@Tag(name = "Auth Controller", description = "Controller for login and logout")
 public class AuthController {
     static final String REST_URL = "/auth";
 
@@ -27,8 +33,13 @@ public class AuthController {
     private final TokenService tokenService;
     private final AuthService authService;
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     @Transactional
+    @Operation(summary = "Login", description = "Use this endpoint for getting tokens")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successfully"),
+            @ApiResponse(responseCode = "401", description = "Wrong credentials our you're blocked"),
+    })
     public TokenResponseDto authenticate(@RequestBody LoginRequestDto loginRequest) {
         String username = loginRequest.getUsername();
         log.info("Attempt to login by user: {}", username);
@@ -45,21 +56,22 @@ public class AuthController {
             return tokenResponseDto;
         } catch (Exception e) {
             bruteForceProtectionService.loginFailed(username);
-            log.info("Wrong credentials for user: {}", username);
-            throw new BadCredentialsException("Wrong credentials");
+            throw e;
         }
     }
 
-    @PostMapping("/logout")
+    @PostMapping(value = "/logout", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Logout", description = "Use this endpoint for invalidation tokens")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Logout successfully"),
+            @ApiResponse(responseCode = "401", description = "Wrong token"),
+    })
     public String logout(@RequestBody TokenResponseDto tokenResponseDto) {
         tokenService.invalidateToken(authService.getJwtToken());
         SecurityContextHolder.clearContext();
 
-        try {
-            tokenService.logoutUser(tokenResponseDto.getRefreshToken());
-        } catch (Exception e) {
-            throw new BadCredentialsException("Invalid refresh token");
-        }
+        tokenService.logoutUser(tokenResponseDto.getRefreshToken());
 
         return "Logged out successfully";
     }
