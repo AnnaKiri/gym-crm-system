412a39158c2cfce988626c071172a9cef741d8f9
Anna Kirillova <a.kirillova1812@gmail.com>
2024-10-22 19:54:46 +0400

    Add unit tests for token service

--- crm-system/src/test/java/com/annakirillova/crmsystem/FeignClientTestData.java
+++ crm-system/src/test/java/com/annakirillova/crmsystem/FeignClientTestData.java
@@ -1,10 +1,13 @@
 package com.annakirillova.crmsystem;
 
+import com.annakirillova.crmsystem.config.KeycloakProperties;
 import com.annakirillova.crmsystem.dto.KeycloakUserDto;
 import com.annakirillova.crmsystem.dto.LoginRequestDto;
 import com.annakirillova.crmsystem.dto.TokenResponseDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.time.Instant;
 import java.util.List;
 
 public class FeignClientTestData {
@@ -13,5 +16,21 @@ public class FeignClientTestData {
     public static final KeycloakUserDto KEYCLOAK_USER_DTO = KeycloakUserDto.builder().id("id").build();
     public static final ResponseEntity<List<KeycloakUserDto>> KEYCLOAK_USER_RESPONSE = ResponseEntity.ok(List.of(KEYCLOAK_USER_DTO));
     public static final MatcherFactory.Matcher<TokenResponseDto> TOKEN_RESPONSE_DTO_MATCHER = MatcherFactory.usingEqualsComparator(TokenResponseDto.class);
+    public static final KeycloakProperties.User USER_PROPERTIES = new KeycloakProperties.User();
+    public static final KeycloakProperties.Admin ADMIN_PROPERTIES = new KeycloakProperties.Admin();
+    public static final Jwt JWT_DUMB = Jwt.withTokenValue("access-token")
+            .header("alg", "none")
+            .claim("exp", Instant.now().plusSeconds(3600))
+            .build();
+
+    static {
+        USER_PROPERTIES.setClientId("user-client");
+        USER_PROPERTIES.setClientSecret("user-secret");
+
+        ADMIN_PROPERTIES.setClientId("admin-client");
+        ADMIN_PROPERTIES.setClientSecret("admin-secret");
+        ADMIN_PROPERTIES.setUsername("admin");
+        ADMIN_PROPERTIES.setPassword("admin-password");
+    }
 
 }
