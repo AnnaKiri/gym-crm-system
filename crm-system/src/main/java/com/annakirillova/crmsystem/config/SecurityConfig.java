62f1110e87980ef7bec7635fc3aa04d7448b4eda
Anna Kirillova <a.kirillova1812@gmail.com>
2024-12-05 16:20:30 +0400

    Module 04.05: Elastic Compute Services Sub-task 1

--- crm-system/src/main/java/com/annakirillova/crmsystem/config/SecurityConfig.java
+++ crm-system/src/main/java/com/annakirillova/crmsystem/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -64,6 +63,7 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation(keycloakProperties.getUrl() + "/realms/" + keycloakProperties.getRealm());
+//        return JwtDecoders.fromIssuerLocation(keycloakProperties.getUrl() + "/realms/" + keycloakProperties.getRealm());
+        return token -> null;
     }
 }
