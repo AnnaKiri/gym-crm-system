798e64683b454d1a3b52503b8864e03f5261fe8a
Anna Kirillova <a.kirillova1812@gmail.com>
2024-12-13 13:30:08 +0400

    Trainer Workload Service mock MongoDB

--- trainer-workload-service/src/main/java/com/annakirillova/trainerworkloadservice/repository/TrainerRepository.java
+++ trainer-workload-service/src/main/java/com/annakirillova/trainerworkloadservice/repository/TrainerRepository.java
@@ -2,10 +2,12 @@ package com.annakirillova.trainerworkloadservice.repository;
 
 import com.annakirillova.trainerworkloadservice.exception.NotFoundException;
 import com.annakirillova.trainerworkloadservice.model.TrainerSummary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
+@Profile("!dev")
 public interface TrainerRepository extends MongoRepository<TrainerSummary, String> {
 
     Optional<TrainerSummary> findByUsername(String username);
